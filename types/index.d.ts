1b075d3b04be0cf976976c54a75c5bbfa729ea0d
itihon <kalabin.alexandr@gmail.com>
2025-03-23 22:19:51 +0500

    chore: generate types

---
+++ types/index.d.ts
@@ -0,0 +1,27 @@
+/**
+ * @callback RequestAnimationFrameStateCallback
+ * @param {*} ctx
+ * @param {RequestAnimationFrameLoop} loop
+ * @param {number} timestamp
+ */
+export default class RequestAnimationFrameLoop {
+    constructor(ctx: any);
+    /**
+     * @param {RequestAnimationFrameStateCallback} cb
+     * @returns {RequestAnimationFrameLoop}
+     */
+    started(cb: RequestAnimationFrameStateCallback): RequestAnimationFrameLoop;
+    /**
+     * @param {RequestAnimationFrameStateCallback} cb
+     * @returns {RequestAnimationFrameLoop}
+     */
+    each(cb: RequestAnimationFrameStateCallback): RequestAnimationFrameLoop;
+    /**
+     * @param {RequestAnimationFrameStateCallback} cb
+     * @returns {RequestAnimationFrameLoop}
+     */
+    stopped(cb: RequestAnimationFrameStateCallback): RequestAnimationFrameLoop;
+    start(): void;
+    stop(): void;
+}
+export type RequestAnimationFrameStateCallback = (ctx: any, loop: RequestAnimationFrameLoop, timestamp: number) => any;
